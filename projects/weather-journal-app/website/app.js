e701eb9443f1a59a279566c1ddf12209fed55e0a
beshara-fakhry <69895939+beshara-fakhry@users.noreply.github.com>
2020-09-08 12:34:01 +0200

    Update app.js

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -3,3 +3,21 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+let baseURL='http://api.openweathermap.org/data/2.5/weather?q=';
+let apiKey='&APPID=26b4b3725425d3561f3fa5678b269ca7';
+const generate=document.getElementById('generate');
+
+generate.addEventListener('click',make);
+
+function make(e){
+ const zip=document.getElementById('zip);
+ const userFeeling=
+
+
+
+
+
+
+
+};
