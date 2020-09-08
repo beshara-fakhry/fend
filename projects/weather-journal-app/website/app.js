9fefab9fe1573e063d9d6651a61e22f2f3f9de41
beshara-fakhry <69895939+beshara-fakhry@users.noreply.github.com>
2020-09-08 13:47:32 +0200

    Update app.js

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -11,13 +11,47 @@ const generate=document.getElementById('generate');
 generate.addEventListener('click',make);
 
 function make(e){
- const zip=document.getElementById('zip);
- const userFeeling=
+ const zip=document.getElementById('zip');
+ const userFeeling=document.getElementById('feelings');
 
+ getWeatherData(baseURL,zip,apiKey)
 
+ .then(function(data){
+     postData('/add',{temperature:data.temperature,date:data.date,userFeeling:userFeeling});
+     updateUI();
+ };)
+};
 
+const getWeatherData=async (baseURL,zip,apiKey){
+ const response=await fetch(baseURL+zip+apiKey);
+ try{
+  const data=await response.json();
+  return data;
+ }
+ catch(error){console. log('error',error);}
+};
 
+const postData=async (url='',data={})=>{
+ const response=await fetch(url,{
+  method:'POST',
+  credentials:'same-origin',
+  headers:{'Content-Type':'application/json'},
+  body:JSON.stringify(data),
+ });
+ try{
+  const newData=response.json();
+  return newData;
+ }
+ catch(error){console.log('error',error);}
+};
 
-
-
+const UpdateUI=async ()=>{
+ const request=await fetch('/all');
+ try{
+  const allData=await request.json();
+  document.getElementById('date').innerHTML=allData[0].date;
+  document.getElementById('temp').innerHTML=allData[0].temperature;
+  document.getElementById('content').innerHTML=allData[0].userFeeling;
+ }
+ catch(error){console.log('error',error);}
 };
