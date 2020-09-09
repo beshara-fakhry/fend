4325dc528f83a84208f223c91807db9e0d8620cb
beshara-fakhry <69895939+beshara-fakhry@users.noreply.github.com>
2020-09-09 16:32:58 +0200

    Update app.js

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -4,7 +4,7 @@
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-let baseURL='http://api.openweathermap.org/data/2.5/weather?q=';
+let baseURL='http://api.openweathermap.org/data/2.5/weather?zip=';
 let apiKey='&APPID=26b4b3725425d3561f3fa5678b269ca7';
 const generate=document.getElementById('generate');
 
