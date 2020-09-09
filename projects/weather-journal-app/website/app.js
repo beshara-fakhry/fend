074b949d5a096c2ed2f7ef146ec9eeeb4bed9467
beshara-fakhry <69895939+beshara-fakhry@users.noreply.github.com>
2020-09-09 20:49:55 +0200

    Update app.js

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -4,8 +4,8 @@
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-let baseURL='http://api.openweathermap.org/data/2.5/weather?zip=';
-let apiKey='&APPID=26b4b3725425d3561f3fa5678b269ca7';
+const baseURL='http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey='&APPID=26b4b3725425d3561f3fa5678b269ca7&units=imperial';
 const generate=document.getElementById('generate');
 
 generate.addEventListener('click',make);
@@ -19,10 +19,10 @@ function make(e){
  .then(function(data){
      postData('/add',{temperature:data.main.temp,date:newDate,userFeeling:userFeeling});
      updateUI();
- };)
+ });
 };
 
-const getWeatherData=async (baseURL,zip,apiKey){
+const getWeatherData=async (baseURL,zip,apiKey)=>{
  const response=await fetch(baseURL+zip+apiKey);
  try{
   const data=await response.json();
@@ -49,9 +49,9 @@ const updateUI=async ()=>{
  const request=await fetch('/all');
  try{
   const allData=await request.json();
-  document.getElementById('date').innerHTML=allData[0].date;
-  document.getElementById('temp').innerHTML=allData[0].temperature;
-  document.getElementById('content').innerHTML=allData[0].userFeeling;
+  document.getElementById('date').innerHTML=allData.date;
+  document.getElementById('temp').innerHTML=allData.temperature;
+  document.getElementById('content').innerHTML=allData.userFeeling;
  }
  catch(error){console.log('error',error);}
 };
